7a330c5351783723d982b13e8e47ec7847a3ded1
LAlves91 <lincoln.a.m.c@gmail.com>
2015-06-23 22:15:52 -0500

    Update face_recognizer.py

--- face_recognizer.py
+++ face_recognizer.py
@@ -2,8 +2,11 @@
 
 # Import the required modules
 import cv2, os
+import paho.mqtt.client as mqtt
 import numpy as np
 from PIL import Image
+
+
 
 # For face detection we will use the Haar Cascade provided by OpenCV.
 cascadePath = "haarcascade_frontalface_default.xml"
@@ -12,6 +15,42 @@ faceCascade = cv2.CascadeClassifier(cascadePath)
 # For face recognition we will the the LBPH Face Recognizer 
 recognizer = cv2.createLBPHFaceRecognizer()
 
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+		
+def on_message(client, userdata, rc):
+	# Receive the image and create a file to process
+	print("Message received! ")
+	with open("img.jpg","wb") as fd:
+		fd.write(rc.payload)
+	
+	# Set threshold for the confidence
+	thresh = 50.0
+	
+	# Open the image properly for the face detection
+	predict_image_pil = Image.open('img.jpg').convert('L')
+	predict_image = np.array(predict_image_pil, 'uint8')
+	
+	# Uses the cascade to detect a face on the picture
+	faces = faceCascade.detectMultiScale(predict_image)
+	
+	# Applies the recognition to the frame where the face was detected
+	for (x, y, w, h) in faces:
+		nbr_predicted, conf = recognizer.predict(predict_image[y: y + h, x: x + w])
+		
+		# As confidence is actually the Distance, we subtract it of 100 to create a more logical confidence
+		conf100 = 100.0 - conf
+		
+		# If the confidence is above a threshold, the name is sent to the robot, if not, a string 'Null' is sent
+		if conf > thresh:
+			#Find name that matches the key predicted.
+			name = 'Name'
+			cli.publish('response',name,1)
+		else:
+			cli.publish('response','Null',1)
+			
+		
+	
 def get_images_and_labels(path):
     # Append all the absolute image paths in a list image_paths
     # We will not read the image with the .sad extension in the training set
@@ -47,25 +86,21 @@ path = './yalefaces'
 # Call the get_images_and_labels function and get the face images and the 
 # corresponding labels
 images, labels = get_images_and_labels(path)
-cv2.destroyAllWindows()
+#cv2.destroyAllWindows()
 
 # Perform the tranining
 recognizer.train(images, np.array(labels))
+print 'Training complete!'
+# Connect to MQTT server and wait for images
+cli = mqtt.Client("Receiver",1883)
+cli.on_message = on_message
+cli.on_connect = on_connect
+
+cli.connect("54.173.42.47")
+
+cli.subscribe("presence",2)
+
+cli.loop_forever()
 
 
 
-# Append the images with the extension .sad into image_paths
-image_paths = [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.sad')]
-for image_path in image_paths:
-    predict_image_pil = Image.open(image_path).convert('L')
-    predict_image = np.array(predict_image_pil, 'uint8')
-    faces = faceCascade.detectMultiScale(predict_image)
-    for (x, y, w, h) in faces:
-        nbr_predicted, conf = recognizer.predict(predict_image[y: y + h, x: x + w])
-        nbr_actual = int(os.path.split(image_path)[1].split(".")[0].replace("subject", ""))
-        if nbr_actual == nbr_predicted:
-            print "{} is Correctly Recognized with confidence {}".format(nbr_actual, conf)
-        else:
-            print "{} is Incorrect Recognized as {}".format(nbr_actual, nbr_predicted)
-        cv2.imshow("Recognizing Face", predict_image[y: y + h, x: x + w])
-        cv2.waitKey(1000)
